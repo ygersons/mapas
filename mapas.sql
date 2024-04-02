ebc196df871c9c511d1c6c9cd39088200ab79186
ygersons <165719256+ygersons@users.noreply.github.com>
2024-04-02 16:40:33 -0500

    Update mapas.sql

--- mapas.sql
+++ mapas.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost:3306
--- Tiempo de generación: 02-04-2024 a las 17:47:51
--- Versión del servidor: 10.6.17-MariaDB
--- Versión de PHP: 8.1.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `ixocakuy_mapas`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `sml_access_log`
---
 
 CREATE TABLE `sml_access_log` (
   `id` bigint(20) UNSIGNED NOT NULL,
